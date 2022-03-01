b84e711e33db4a14cd4135804f412b80e761eb15
Soufiene Hmila <soufiene.hmila@gmail.com>
2022-03-01 08:39:00 +0100

    update commit

---
+++ lib/screen/screen_home/widget/item_custom_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ItemCustomWidget extends StatefulWidget {
+
+  const ItemCustomWidget({Key? key}) : super(key: key);
+
+  @override
+  _ItemCustomWidgetState createState() => _ItemCustomWidgetState();
+}
+
+class _ItemCustomWidgetState extends State<ItemCustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
